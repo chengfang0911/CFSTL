8dc1d98534d411a040a3aae618cf2a2a40b4223b
fang_cheng <fang_cheng@foxitsoftware.cn>
2018-03-30 18:52:04 +0800

    add Longest_Valid_Parentheses.

--- googletest/test/gtest_prictise.cpp
+++ googletest/test/gtest_prictise.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class Solution {
+class Reverse_Polish_Notation {
 public:
 	int evalRPN(vector<string>& tokens)
 	{
@@ -19,21 +19,94 @@ public:
 		if (oper == "-") return first - second;
 		if (oper == "*") return first * second;
 		if (oper == "/") return first / second;
+		return 0;
 	}
 };
 
 //["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
 //["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
 
-TEST(CPrictise, Reverse_Polish_Notation)
+TEST(Leetcode, Reverse_Polish_Notation)
 {
 	vector<string> tokens;
 	tokens.push_back("3");
 	tokens.push_back("-4");
 	tokens.push_back("+");
 
-
-	Solution su;
+	Reverse_Polish_Notation su;
 	cout << "value is :" << su.evalRPN(tokens) << endl;
 }
 
+class Longest_Valid_Parentheses {
+public:
+	int longestValidParentheses1(string s)
+	{
+		int nlen = s.length();
+		int max = 0;
+		int ntemp = 0;;
+		stack<int> intStack;
+		intStack.push(-1);
+		for (int i = 0; i < nlen; i++)
+		{
+			if (s[i] == '(')
+				intStack.push(i);
+			else
+			{
+	
+				intStack.pop();
+				if (intStack.empty())
+				{
+					intStack.push(i);
+				}
+				else
+				{
+					ntemp = intStack.top();
+					ntemp = i - ntemp;
+					max = max > ntemp ? max : ntemp;
+				}
+			}
+		}
+		return max;
+	}
+	int longestValidParentheses2(string s)
+	{
+		int nleft = 0;
+		int nright = 0;
+		int max = 0;
+		int nlen = s.length();
+		for (int i = 0; i < nlen; i++)
+		{
+			if (s[i] == '(')
+				nleft++;
+			else
+				nright++;
+		
+			if (nleft == nright)
+				max = max > nleft * 2 ? max : nleft * 2;
+			else if(nright >= nleft)
+				nright = nleft = 0;
+		}
+		nright = nleft = 0;
+		for (int i = nlen - 1; i >= 0; i--)
+		{
+			if (s[i] == ')')
+				nright++;
+			else
+				nleft++;
+		
+			if (nleft == nright)
+				max = max > nleft * 2 ? max : nleft * 2;
+			else if(nleft >= nright)
+				nleft = nright = 0;
+		}
+		return max;
+	}
+};
+
+TEST(Leetcode, Longest_Valid_Parentheses)
+{
+	Longest_Valid_Parentheses lvp;
+	cout << lvp.longestValidParentheses1(")()())()()(") << endl;
+	cout << lvp.longestValidParentheses2(")()())()()(") << endl;
+
+}
