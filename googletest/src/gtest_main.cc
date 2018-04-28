ba073f0c7f57a738858f96dfff7dd21de026a9ea
fang_cheng <fang_cheng@foxitsoftware.cn>
2018-04-28 15:08:05 +0800

    add gtest_stl_unittest project.

--- googletest/src/gtest_main.cc
+++ googletest/src/gtest_main.cc
@@ -34,7 +34,7 @@
 GTEST_API_ int main(int argc, char **argv) {
   printf("Running main() from gtest_main.cc\n");
 
-  testing::GTEST_FLAG(filter) = "Leetcode.Zig";
+  testing::GTEST_FLAG(filter) = "Leetcode.longestCommonPrefix";
 
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
--- src/Linklist/LinkList.cpp
+++ src/Linklist/LinkList.cpp
@@ -3,7 +3,7 @@
 // stdafx.obj will contain the pre-compiled type information
 
 
-#include "LinkList.h"
+#include "../../include/Linklist/LinkList.h"
 
 // TODO: reference any additional headers you need in STDAFX.H
 // and not in this file
